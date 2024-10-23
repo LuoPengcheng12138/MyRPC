23b06552d74d8cee1c7403df11470efdcf8e92a4
LuoPengcheng <2653972504@qq.com>
2024-10-24 00:27:46 +0800

    RPCVersion4.1

---
+++ src/main/java/com/luopc/myrpcversion5/register/ZkServiceRegister.java
@@ -0,0 +1,66 @@
+package com.luopc.myrpcversion5.register;
+
+import org.apache.curator.RetryPolicy;
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.retry.ExponentialBackoffRetry;
+import org.apache.zookeeper.CreateMode;
+
+import java.net.InetSocketAddress;
+import java.util.List;
+
+public class ZkServiceRegister implements ServiceRegister {
+    // curator 提供zookeeper客户端
+    private CuratorFramework client;
+    // zookeeper 根路径节点
+    private static final String ZK_ROOT_PATH = "/my-rpc-version5";
+
+    public ZkServiceRegister() {
+        RetryPolicy retryPolicy = new ExponentialBackoffRetry(1000, 3);
+        this.client= CuratorFrameworkFactory.builder().connectString("127.0.0.1:2181")
+                .sessionTimeoutMs(40000).retryPolicy(retryPolicy).namespace(ZK_ROOT_PATH).build();
+        this.client.start();
+        System.out.println("zookeeper service connect ...");
+    }
+    @Override
+    public void register(String serviceName, InetSocketAddress serverAddress){
+        try{
+            // serviceName创建成永久节点，服务提供者下线时，不删服务名，只删地址
+            if(client.checkExists().forPath("/"+serviceName)==null){
+                client.create().creatingParentsIfNeeded().withMode(CreateMode.PERSISTENT).forPath("/"+serviceName);
+            }
+            // 地址路径，一个/代表一个节点
+            String path="/"+serviceName+"/"+getServiceAddress(serverAddress);
+            // 临时节点，服务器下线就删除节点
+            client.create().creatingParentsIfNeeded().withMode(CreateMode.EPHEMERAL).forPath(path);
+        } catch (Exception e) {
+            System.out.println("this service is exist");
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public InetSocketAddress serviceDiscovery(String serviceName){
+        try{
+            List<String> children = client.getChildren().forPath("/"+serviceName);
+            //这里默认用的第一个，后面加负载均衡
+            String string=children.get(0);
+            return parseAddress(string);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+
+    // 地址 -> XXX.XXX.XXX.XXX:port 字符串
+    private String getServiceAddress(InetSocketAddress serverAddress){
+        return serverAddress.getHostName()+":"+serverAddress.getPort();
+    }
+    //字符串解析为地址
+    private InetSocketAddress parseAddress(String address){
+        String[] split = address.split(":");
+        return new InetSocketAddress(split[0], Integer.parseInt(split[1]));
+    }
+
+}
